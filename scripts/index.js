6cc5673a3e63eaf65d0d7fbff9b6e210e3d988e2
reactNoobie <reactnoobie@gmail.com>
2020-09-08 00:56:22 +0600

    Added a lot of stuffs learned from google dev docs
    * Used TweenMax to animate from JS
    * Slide in / out (carousel) using absolute positioning
    * Animating modal views using fixed positioning

--- scripts/index.js
+++ scripts/index.js
@@ -19,7 +19,7 @@ const render = () => {
             pausable.classList.toggle('paused');
         }
     });
-}
+};
 
 const clickbait = document.querySelector('.clickbait');
 clickbait.onclick = () => {
@@ -36,6 +36,77 @@ containerGrid.onclick = () => {
     emergingBox.classList.add('pausable');
     containerGrid.appendChild(emergingBox);
     render();
-}
+};
+
+const animateJsAnimation = () => {
+    const jsAnimation = document.querySelector('.js-animation');
+    jsAnimation.animate([{ backgroundColor: 'turquoise' }, { backgroundColor: 'teal' }], 3000);
+};
+
+const animateJsAnimationButton = document.querySelector('.js-animation-btn');
+animateJsAnimationButton.onclick = animateJsAnimation;
+
+const animateTweenMaxExample = () => {
+    const tweenMaxExample = document.querySelector('.tweenmax-example');
+    TweenMax.to(
+        tweenMaxExample,
+        2,
+        {
+            y: '100%',
+            ease: 'Elastic.easeOut',
+            repeat: 1,
+            yoyo: true,
+        },
+    );
+};
+
+const tweenMaxExampleButton = document.querySelector('.tweenmax-example-btn');
+tweenMaxExampleButton.onclick = animateTweenMaxExample;
+
+const itemNames = document.querySelectorAll('.item-name');
+itemNames.forEach((itemName, index) => {
+    itemName.onclick = () => {
+        document.querySelector('.item-list').style.transform = 'translateX(-100%)';
+        document.querySelector(`.item-description:nth-of-type(${index + 1})`).style.transform = 'translateX(0)';
+    }
+});
+
+const itemDescriptions = document.querySelectorAll('.item-description');
+itemDescriptions.forEach(itemDescription => {
+    itemDescription.onclick = () => {
+        itemDescription.style.transform = 'translateX(100%)';
+        document.querySelector('.item-list').style.transform = 'translateX(0)';
+    }
+});
+
+
+const CAROUSEL_ITEM_COUNT = 5;
+const updateCarouselControlButtons = () => {
+    slideLeftButton.disabled = carouselIndex === 0;
+    slideRightButton.disabled = carouselIndex === CAROUSEL_ITEM_COUNT - 1;
+};
+
+const carouselItems = document.querySelectorAll('.carousel-container>div');
+const slide = slideTo => {
+    if (slideTo === 'right') carouselIndex++;
+    else carouselIndex--;
+    updateCarouselControlButtons();
+    carouselItems.forEach(item => item.style.transform = `translateX(calc(-100% * ${carouselIndex}))`);
+};
+
+let carouselIndex = 0;
+const slideLeftButton = document.querySelector('.slide-left-btn');
+slideLeftButton.onclick = slide.bind(null, 'left');
+const slideRightButton = document.querySelector('.slide-right-btn');
+slideRightButton.onclick = slide.bind(null, 'right');
+updateCarouselControlButtons();
+
+document.querySelector('.show-modal-btn').onclick = () => {
+    document.querySelector('.modal-container').classList.add('shown');
+};
+
+document.querySelector('.dismiss-modal').onclick = () => {
+    document.querySelector('.modal-container').classList.remove('shown');
+};
 
 render();
